ce1fb59b9557b57aa6fd26622445cb5eb318a347
Valentin Coulon <Valentin.coulon5@outlook.fr>
2025-09-24 10:55:35 +0200

    Add RecapController and recap index view

---
+++ App_tp/Controllers/RecapController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace App_tp.Controllers
+{
+    public class RecapController : Controller
+    {
+        // GET: Recap
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
