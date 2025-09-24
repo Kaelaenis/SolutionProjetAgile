929bdcd893ae52a6f5855de249bdfd868be29166
Valentin Coulon <Valentin.coulon5@outlook.fr>
2025-09-24 12:37:26 +0200

    Add User Model

---
+++ App_tp/Models/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_tp.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public string Role { get; set; }
+
+        public User() { }
+        public User(int id, string name, string firstName, string role)
+        {
+            Id = id;
+            Name = name;
+            FirstName = firstName;
+            Role = role;
+        }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}, Name: {Name}, FirstName: {FirstName}, Role: {Role}";
+
+        }
+    }
+}
